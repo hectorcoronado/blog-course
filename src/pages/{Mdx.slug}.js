448b82cb1c53d4cde424dd9b0d47929f82a62a96
Hector Coronado <hector.coronado@autodesk.com>
2021-03-30 11:19:50 -0700

    add body to graphql query & mdxrenderer in mdx.slug.js

---
+++ src/pages/{Mdx.slug}.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {graphql} from 'gatsby'
+import {MDXRenderer} from 'gatsby-plugin-mdx'
+
+import Layout from '../components/Layout'
+
+const BlogPostPage = ({data}) => {
+  const post = data.mdx
+  
+  return(
+    <Layout>
+      <article>
+        <h2>{post.frontmatter.title}</h2>
+        <MDXRenderer>
+          {post.body}
+        </MDXRenderer>
+      </article>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query BlogPostById($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+      }
+      body
+    }
+  }
+`
+
+export default BlogPostPage
