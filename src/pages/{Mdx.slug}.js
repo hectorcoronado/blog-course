4689cbecabd7f832cce9e30b97d2087f8e06ce6b
Hector Coronado <hector.coronado@autodesk.com>
2021-03-31 13:17:44 -0700

    install & use gatsbyimage component

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import {graphql} from 'gatsby'
 import {MDXRenderer} from 'gatsby-plugin-mdx'
+import {GatsbyImage, getImage} from 'gatsby-plugin-image'
 
 import Layout from '../components/Layout'
 
 const BlogPostPage = ({data}) => {
   const post = data.mdx
+  const image = getImage(post.frontmatter.image)
   
   return(
     <Layout>
       <article>
+        <GatsbyImage
+          image={image}
+          alt={post.frontmatter.imageAlt}
+        />
         <h2>{post.frontmatter.title}</h2>
         <MDXRenderer>
           {post.body}
@@ -24,6 +30,12 @@ export const query = graphql`
     mdx(id: {eq: $id}) {
       frontmatter {
         title
+        image {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED)
+          }
+        }
+        imageAlt
       }
       body
     }
